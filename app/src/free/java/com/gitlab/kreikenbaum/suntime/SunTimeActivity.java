356b72b1fa2ca7956eae547269be3b5be6e6f0cb
serv-inc <serv-inc@users.noreply.github.com>
2019-04-21 16:15:17 +0200

    bug: had unused settings menu entry

--- app/src/free/java/com/gitlab/kreikenbaum/suntime/SunTimeActivity.java
+++ app/src/free/java/com/gitlab/kreikenbaum/suntime/SunTimeActivity.java
@@ -15,8 +15,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.RemoteViews;
 import android.widget.TextClock;
 import android.widget.Toast;
@@ -110,28 +108,6 @@ public class SunTimeActivity extends AppCompatActivity
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOC_CODE);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sun_time, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressLint("MissingPermission")
--- app/src/paid/java/com/gitlab/kreikenbaum/suntime/SunTimeActivity.java
+++ app/src/paid/java/com/gitlab/kreikenbaum/suntime/SunTimeActivity.java
@@ -124,27 +124,6 @@ public class SunTimeActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sun_time, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressLint("MissingPermission")
     @Override public void onConnected(Bundle bundle) {
