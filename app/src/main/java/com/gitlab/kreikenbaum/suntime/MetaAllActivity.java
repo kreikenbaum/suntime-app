9a8ac3c523dee3aff60668c38e8747a544c3728b
Michael <kreikenbaum@users.noreply.github.com>
2019-06-20 17:06:56 +0200

    feat: lacked menu UI

--- app/src/main/java/com/gitlab/kreikenbaum/suntime/MetaAllActivity.java
+++ app/src/main/java/com/gitlab/kreikenbaum/suntime/MetaAllActivity.java
@@ -2,6 +2,8 @@ package com.gitlab.kreikenbaum.suntime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /** sets up menu */
 public class MetaAllActivity extends AppCompatActivity {
@@ -9,5 +11,11 @@ public class MetaAllActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.all_menu, menu);
+        return true;
     }
 }
